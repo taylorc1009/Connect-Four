7df529388870980772f4df1a3dcd19711ae289e2
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-03-23 13:55:49 +0000

    Fixed buffer cleaner, converted goto loops into while loops for efficiency - next job is to convert validateOption input method to fgets

--- connect.c
+++ connect.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NAME_MAX 32 // 2 extra to accomodate for '\0' and '\n'
+#define NAME_MAX 32 // 2 extra bytes to accomodate '\0' and '\n', so remember to subtract 2 during char* length checks
+#define NUL '\0' // used to "nullify" a char
 
 typedef enum { false, true } bool;
 
@@ -15,11 +16,12 @@ struct Settings {
 
 void setup(struct Settings *settings);
 
-void clean_stdin(void) { // causes app to hang
-	int c;
-	do {
-		c = getchar();
-	} while (c != '\n' && c != EOF);
+static inline void cleanStdin()
+{
+	char c = NUL;
+	while ((c = getchar()) != '\n' && c != EOF) {
+		// Do nothing until input buffer is fully flushed. 
+	}
 }
 
 int main(int argc, char **argv) {
@@ -33,14 +35,18 @@ int main(int argc, char **argv) {
 
 	printf("Welcome to Connect 4! Reproduced virtually using C by Taylor Courtney - 40398643\nTo begin, select either:\n\n 1 - how it works\n 2 - change board size (currently %dx%d)\n 3 - start Player versus Player\n 4 - start Player versus AI\n 5 - quit", settings->boardX, settings->boardY);
 	
-	mainmenu:
+	//mainmenu:
+	int option = 0;
+	while (option == 0) {
 		settings->solo = false;
 		printf("\n\nWhat would you like to do?\n> ");
-		int option = validateOption(1, 5);
+		option = validateOption(1, 5);
 		switch (option) {
 			case 1:
-				printf("\nWelcome to Connect 4! Reproduced virtually using C by Taylor Courtney - 40398643\nTo begin, select either:\n\n 1 - how it works\n 2 - change board size (currently %dx%d)\n 3 - start Player versus Player\n 4 - start Player versus AI\n 5 - quit\n\nConnect 4 is a rather simple game. Both users take a turn each selecting a column\nwhich they would like to drop their token (player 1 = O, player 2 = X) into next.\n\nThis continues until one player has connected 4 of their tokens in a row either\nhorizontally, vertically or diagonally.", settings->boardX, settings->boardY);
-				goto mainmenu;
+				printf("\nConnect 4 is a rather simple game. Both users take a turn each selecting a column\nwhich they would like to drop their token (player 1 = O, player 2 = X) into next.\n\nThis continues until one player has connected 4 of their tokens in a row either\nhorizontally, vertically or diagonally.", settings->boardX, settings->boardY);
+				//goto mainmenu;
+				option = 0;
+				break;
 
 			case 2:
 				printf("\nPlease enter the width (amount of columns) you want to play with\n> ");
@@ -48,26 +54,33 @@ int main(int argc, char **argv) {
 				printf("\nPlease enter the height (amount of rows) you want to play with\n> ");
 				settings->boardY = validateOption(3, 12);
 				printf("\nBoard dimensions changed successfully to %dx%d", settings->boardX, settings->boardY);
-				goto mainmenu;
+				//goto mainmenu;
+				option = 0;
+				break;
 
 			case 3:
 				setup(settings);
-				goto mainmenu;
+				//goto mainmenu;
+				option = 0;
+				break;
 
 			case 4:
 				settings->solo = true;
 				setup(settings);
-				goto mainmenu;
+				//goto mainmenu;
+				option = 0;
+				break;
+
 			case 5:
 				break;
 		}
-	exit:
-		free(settings->player1);
-		free(settings->player2);
-		free(settings);
-		system("cls");
-		printf("Connect 4 closed, goodbye!\n");
-		return 0;
+	}
+	free(settings->player1);
+	free(settings->player2);
+	free(settings);
+	system("cls");
+	printf("Connect 4 closed, goodbye!\n");
+	return 0;
 }
 
 int validateOption(int min, int max) {
@@ -78,7 +91,7 @@ int validateOption(int min, int max) {
 		num = 0;
 		if (scanf("%d%c", &num, &term) != 2 || term != '\n' || !(num >= min && num <= max)) {
 			printf("\n! invalid input: please re-enter an number between %d and %d\n> ", min, max);
-			clean_stdin();
+			//cleanStdin();
 		} else
 			valid = true;
 	}
@@ -97,29 +110,41 @@ void removeExcessSpaces(char* str) { // used to remove preceeding and exceeding 
 }
 
 void getName(char** player) { // dynamically resizes the allocation of the player name char array based on the input
-	char buffer[NAME_MAX];
-	getname:
-		//clean_stdin();
-		fgets(buffer, sizeof(buffer), stdin);
-		removeExcessSpaces(buffer);
+	char* buffer = (char*)malloc(sizeof(char) * NAME_MAX);
+	memset(buffer, NUL, NAME_MAX);
+
+	char* input = NULL;
+	//getname:
+	while (input == NULL) {
+		
+		input = fgets(buffer, NAME_MAX, stdin);
+		//removeExcessSpaces(input);
 		size_t bufLen = strlen(buffer);
-		if(buffer == 0 || buffer[0] == '\0') {
+		if (buffer == 0 || buffer[0] == '\0') {
 			printf("\n! name empty, please enter one\n> ");
-			goto getname;
-		} else if (bufLen > 30) {
+			//goto getname;
+			input = NULL;
+		}
+		//else if (bufLen > NAME_MAX - 2) {
+		else if (buffer[bufLen - 1] != '\n') {
 			printf("\n! name to long, please re-enter\n> ");
-			goto getname;
-		} else {
-			if (buffer[bufLen - 1] == '\n') {
-				buffer[bufLen - 1] = '\0';
+			cleanStdin();
+			//goto getname;
+			input = NULL;
+		}
+		else {
+			if (input[bufLen - 1] == '\n') {
+				input[bufLen - 1] = '\0';
 				bufLen--;
 			}
 
-			if (bufLen > sizeof(*player)) {
+			if (bufLen > sizeof(*player))
 				*player = (char*)realloc(*player, sizeof(char) * bufLen);
-			}
-			strcpy(*player, buffer);
+
+			strcpy(*player, input);
 		}
+	}
+	free(buffer);
 }
 
 void setup(struct Settings *settings) {
@@ -135,4 +160,5 @@ void setup(struct Settings *settings) {
 		getName(&(settings)->player2);
 		printf("\nWelcome %s and %s!", settings->player1, settings->player2);
 	}
+	//*settings = (struct Settings*)realloc(*settings, sizeof(int) * 2 + sizeof(bool) + sizeof(char) * strlen(settings->player1) + sizeof(char) * strlen(settings->player2));
 }
