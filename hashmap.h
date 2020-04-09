95534c5d8c3ffdde5f1dce7745c2951eec6af3a8
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-04-09 11:57:57 +0100

    Possibly implemented hashmap of stacks structure, next job is to implement adding values to the stacks

--- hashmap.h
+++ hashmap.h
@@ -1,4 +1,64 @@
+// source - http://www.kaushikbaruah.com/posts/data-structure-in-c-hashmap/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+struct node {
+    int key;
+    struct stack* val;
+    struct node* next;
+};
+struct table {
+    int size;
+    struct node** list;
+};
+struct table* createTable(int size) {
+    struct table* t = (struct table*)malloc(sizeof(struct table));
+    t->size = size;
+    t->list = (struct node**)malloc(sizeof(struct node*) * size);
+    for (int i = 0; i < size; i++)
+        t->list[i] = NULL; // this is where we will define the columns as stacks?
+    return t;
+}
+int hashCode(struct table* t, int key) {
+    if (key < 0)
+        return -(key % t->size);
+    return key % t->size;
+}
+void insert(struct table* t, int key, char val) {
+    int pos = hashCode(t, key);
+    struct node* list = t->list[pos];
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp = list;
+    while (temp) {
+        if (temp->key == key) {
+            //temp->val = val;
+            push(temp->val, val);
+            return;
+        }
+        temp = temp->next;
+    }
+    newNode->key = key;
+    //newNode->val = val;
+    push(newNode->val, val);
+    newNode->next = list;
+    t->list[pos] = newNode;
+}
+struct stack* lookup(struct table* t, int key) {
+    int pos = hashCode(t, key);
+    struct node* list = t->list[pos];
+    struct node* temp = list;
+    while (temp) {
+        if (temp->key == key) {
+            return temp->val;
+        }
+        temp = temp->next;
+    }
+    return -1;
+}
+
+/*#include <stdio.h>
 #include <stdlib.h>
 
 struct node {
@@ -14,9 +74,9 @@ struct table* createTable(int size) {
     struct table* t = (struct table*)malloc(sizeof(struct table));
     t->size = size;
     t->list = (struct node**)malloc(sizeof(struct node*) * size);
-    int i;
+    int i; // move inside for loop declaration
     for (i = 0; i < size; i++)
-        t->list[i] = NULL;
+        t->list[i] = NULL; // this is where we will define the columns as stacks?
     return t;
 }
 int hashCode(struct table* t, int key) {
@@ -52,11 +112,4 @@ int lookup(struct table* t, int key) {
         temp = temp->next;
     }
     return -1;
-}/*
-int main() {
-    struct table* t = createTable(5);
-    insert(t, 2, 3);
-    insert(t, 5, 4);
-    printf("%d", lookup(t, 5));
-    return 0;
 }*/
---
+++ stack.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stackNode {
+	char val;
+	struct stackNode* prev;
+	struct stackNode* next;
+};
+struct stack {
+	//int* array;
+	struct stackNode** list;
+	int top;
+	int size;
+};
+
+struct stack* createStack(int size) {
+	struct stack* s = (struct stack*)malloc(sizeof(struct stack));
+	s->top = -1;
+	s->size = size;
+	s->list = (struct stackNode**)malloc(sizeof(struct stackNode*) * size);
+	for (int i = 0; i < size; i++)
+		s->list[i] = NULL;
+	return s;
+}
+void push(struct stack* s, char val) { // return boolean to determine push success
+	if (s->top == s->size - 1) {
+		printf(" Stack is full . Couldn ’t push ’%d’ onto stack \n", val);
+		return;
+	}
+	struct stackNode* newNode = (struct stackNode*)malloc(sizeof(struct stackNode));
+	if (s->top != -1)
+		newNode->prev = s->list[s->top];
+	else
+		newNode->prev = NULL;
+	s->top++;
+	newNode->val = val;
+	newNode->next = NULL;
+	s->list[s->top] = newNode;
+}
+struct stackNode** pop(struct stack* s) {
+	struct stackNode** data;
+	if (s->top == -1) {
+		printf(" Stack is empty \n");
+		return NULL;
+	}
+	data = &s->list[s->top];
+	s->top--;
+	// should the data be freed here? No, we still need this portion of memory to be reserved in case new data is added here later
+	return data;
+}
