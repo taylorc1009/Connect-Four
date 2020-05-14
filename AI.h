34b95a9730f5c6c24653530fb450c4b9f34a11a0
Taylor Courtney <taylorcourtney2000@googlemail.com>
2020-05-14 11:20:09 +0100

    + Added AI vertical score + Commented out debug information outputs

--- AI.h
+++ AI.h
@@ -18,40 +18,60 @@ void delay(int numOfSeconds) {
 void AIMakeMove(struct hashmap* board, int* column) {
 	int x = getX(board), y = getY(board);
 	pickBestMove(board, x, y, column);
-	printf("picked column: %d", *column);
+	//printf("picked column: %d", *column);
 	//delay(30);
 }
 
 void getScore(struct hashmap* board, int x, int y, int* finalScore) { // determines the best column to make a play in by giving each a score based on their current state
 	int score = 0;
 	
-	// horizontal check
+	// horizontal score
 	for (int i = 0; i < y; i++) {
 		int* row = malloc(sizeof(int) * x);
 		
-		printf("\n");
+		//printf("\n");
 		for (int j = 0; j < x; j++) {
 			row[j] = getToken(board, j, i);
-			printf("%d, ", row[j]);
+			//printf("%d, ", row[j]);
 		}
 
 		for (int j = 0; j < x - 3; j++) {
 			int window[4] = { row[j], row[j + 1], row[j + 2], row[j + 3] };
-			printf("window %d = { %d, %d, %d, %d } >> P2 tokens: %d, ", j, row[j], row[j + 1], row[j + 2], row[j + 3], count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)));
-			if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 1 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 3)
-				score += 1;
-			else if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 2 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 2)
+			//printf("window %d = { %d, %d, %d, %d } >> P2 tokens: %d, ", j, row[j], row[j + 1], row[j + 2], row[j + 3], count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)));
+			if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 2 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 2)
 				score += 10;
 			else if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 3 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 1)
 				score += 100;
 			else if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 4)
 				score += 1000;
 		}
-		printf("score: %d", score);
+		//printf("score: %d", score);
 		
 		free(row);
 	}
-	printf("\n");
+
+	// vertical score
+	for (int i = 0; i < x; i++) {
+		int* col = malloc(sizeof(int) * y);
+
+		for (int j = 0; j < y; j++) {
+			col[j] = getToken(board, i, j);
+		}
+
+		for (int j = 0; j < y - 3; j++) {
+			int window[4] = { col[j], col[j + 1], col[j + 2], col[j + 3] };
+			//printf("window %d = { %d, %d, %d, %d } >> P2 tokens: %d, ", j, row[j], row[j + 1], row[j + 2], row[j + 3], count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)));
+			if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 2 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 2)
+				score += 10;
+			else if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 3 && count(window, EMPTY_SLOT, ARRAY_LENGTH(window)) == 1)
+				score += 100;
+			else if (count(window, PLAYER_2_TOKEN, ARRAY_LENGTH(window)) == 4)
+				score += 1000;
+		}
+
+		free(col);
+	}
+	//printf("\n");
 	
 	*finalScore = score;
 }
@@ -88,7 +108,7 @@ void pickBestMove(struct hashmap* board, int x, int y, int* column) {
 
 // ideally we would calculate the array length here, but this isn't possible
 // as the compiler doesn't know what the pointer is pointing to, so it cannot
-// define the length at compile time
+// define the length of the array at compile time
 int count(int* list, int tok, int n) {
 	int c = 0;
 	for (int i = 0; i < n; i++)
--- connect.c
+++ connect.c
@@ -272,8 +272,8 @@ void play(struct Settings* settings) {
 
 void displayBoard(struct hashmap* board) { // add a move down animation?
 	int x = getX(board), y = getY(board), i, j;
-	//system("cls");
-	printf("\n");//
+	system("cls");
+	//printf("\n");//
 
 	for (i = 0; i < y; i++) {
 		printf("+");
