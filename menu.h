fe5dcf088397476734d611b8195afde57e119e95
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-05-01 12:20:22 +0100

    Removed NAME_BUFFER_MAX constant

--- menu.h
+++ menu.h
@@ -1,8 +1,6 @@
 #include <ctype.h>
 #include "game.h"
 
-#define NAME_BUFFER_MAX 32 //2 extra bytes to accomodate '\n' and '\0'
-
 int removeExcessSpaces(char* str);
 char* inputString(FILE* fp, size_t size);
 void getPlayerName(char** name, int* nameSize, int playerNum);
