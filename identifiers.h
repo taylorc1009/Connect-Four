fa20bbcdc826271acb68f4971fb07a8382a9c8bc
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-05-26 13:37:14 +0100

    Improved readability of a comment

--- identifiers.h
+++ identifiers.h
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//we use a typedef instead of '#include<stdbool.h>' as "bool" is not a key-word to the compiler
-//so including it when we use it as a return type gives errors
+//we use a typedef instead of '#include<stdbool.h>' as "bool" is not a key-word to the compiler, so including it when we use it as a return type gives unrecognised return type errors
 typedef enum { false, true } bool;
 
 #define PLAYER_1_TOKEN 1
